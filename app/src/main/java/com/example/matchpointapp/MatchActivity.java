fe812b5a884813c4e2a73630da28b0d8375849f1
German Suárez <germancloud@gmail.com>
2021-10-17 11:16:20 -0500

    Project creation and design activities.

---
+++ app/src/main/java/com/example/matchpointapp/MatchActivity.java
@@ -0,0 +1,15 @@
+package com.example.matchpointapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match2);
+    }
+}
---
+++ app/src/main/java/com/example/matchpointapp/entity/Team.java
@@ -0,0 +1,4 @@
+package com.example.matchpointapp.entity;
+
+public class Team {
+}
